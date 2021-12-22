951b4e3256d8784d7234528b65c2f95f4f69b059
GitHub Action <action@github.com>
2021-12-22 09:28:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/nevermere/androidtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nevermere/androidtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nevermere.androidtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nevermere.androidtemplate.MyBundle
 
 class MyApplicationService {
 
